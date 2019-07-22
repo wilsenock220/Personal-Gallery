55af486de70db184b7526253d33c3f7e8163973f
wilsenock220 <wilsenock220@gmail.com>
2019-07-22 11:45:25 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -5,25 +5,16 @@ from decouple import config, Csv
 MODE = config('MODE', default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-​
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-​
-​
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-​
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '6(d1m=h9ydgwk63amqc+emgngy)yvh!3r*mn3l-f9n&h9(swk='
-​
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-​
 ALLOWED_HOSTS = []
-​
-​
 # Application definition
-​
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -33,7 +24,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'personal',
 ]
-​
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -44,9 +34,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-​
 ROOT_URLCONF = 'gallery.urls'
-​
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -63,13 +51,9 @@ TEMPLATES = [
         },
     },
 ]
-​
 WSGI_APPLICATION = 'gallery.wsgi.application'
-​
-​
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-​
 # Development
 # if config('MODE')=="dev":
 #        DATABASES = {
@@ -81,7 +65,6 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 #            'HOST': config('DB_HOST'),
 #            'PORT': '',
 #        }
-
 #    }
 # production
 # else:
@@ -90,17 +73,11 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-​
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-​
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-​
-​
-​
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-​
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -115,43 +92,28 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-​
-​
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
-​
 LANGUAGE_CODE = 'en-us'
-​
 TIME_ZONE = 'Africa/Nairobi'
-​
 USE_I18N = True
-​
 USE_L10N = True
-​
 USE_TZ = True
-​
-​
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-​
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 # ]
-​
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-​
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-​
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-​
 # configuring the location for media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-​
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
